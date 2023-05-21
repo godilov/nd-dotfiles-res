68ccaef6513dccf577ca2a055f247727da676b3c
German Odilov <german.odilov@outlook.com>
2023-05-21 18:55:59 +0600

    nd.res.core.color.awesome: added accents

--- src/nd/res/core/color/awesome/accent/main.lua
+++ src/nd/res/core/color/awesome/accent/main.lua
@@ -1,4 +1,20 @@
+local type_lib   = require 'nd.lib.core.type'
+local assert_lib = require 'nd.lib.core.assert'
+
+local is_tab     = type_lib.is_tab
+
+local nd_assert  = assert_lib.get_fn(ND_RES_IS_DEBUG)
+local nd_err     = assert_lib.get_err_fn 'nd.res.core.color.awesome.accent.main'
+
 return function(config)
+    local palette = config.palette
+
+    nd_assert(is_tab(palette), nd_err, 'fn(): palette must be of type table')
+
     return {
+        core     = palette.black,
+        emphasis = palette.yellow,
+        focus    = palette.red,
+        text     = palette.white,
     }
 end
